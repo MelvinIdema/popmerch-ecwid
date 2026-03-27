6e69fb66375bca84e5df1e3141cbfb8531b16d7e
MelvinIdema <melvin_i13@hotmail.com>
2026-03-27 08:11:42 +0100

    Disable currency selector & add debug flag for testing

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import { initCurrencySwitcher } from "./currency-switcher.js";
 const GEOAPIFY_API_KEY = "c70aedc3c26e44238b962936e3757ec4";
 const BUNDLE_VERSION = "2026-03-21-inline-checkout-5";
 const ENABLE_ADDRESS_VALIDATION = true;
-const ENABLE_CURRENCY_SWITCHER = true;
+const ENABLE_CURRENCY_SWITCHER = localStorage.getItem("ENABLE_CURRENCY_SWITCHER") === "true" || false;
 
 function safeInit(name, init) {
   try {
